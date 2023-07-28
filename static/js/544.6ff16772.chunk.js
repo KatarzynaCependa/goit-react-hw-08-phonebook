649b0386f636c8200a13983c7158123d6f90a5c9
KatarzynaCependa <116761001+KatarzynaCependa@users.noreply.github.com>
2023-07-28 08:28:54 +0000

    Deploying to gh-pages from @ KatarzynaCependa/goit-react-hw-08-phonebook@c76c5a8364c38e06e5cd48b81be64babcbc374f6 🚀

---
+++ static/js/544.6ff16772.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[544],{9544:function(e,o,n){n.r(o);var t=n(184);o.default=function(){return(0,t.jsx)("div",{children:(0,t.jsx)("h1",{children:"Hello moto!"})})}}}]);
+//# sourceMappingURL=544.6ff16772.chunk.js.map
